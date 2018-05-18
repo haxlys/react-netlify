76e0f653d46dcfac1effbdb1a39dd1df5169900a
haxlys <haxlys@gmail.com>
2018-05-19 01:03:41 +0900

    code split

---
+++ src/client/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import App from 'shared/App';
+import Menu from 'components/Menu';
+
+const Root = () => (
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+);
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Root from './client/Root';
 import registerServiceWorker from './registerServiceWorker';
+import { AppContainer } from 'react-hot-loader';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
+
+render(Root)
+
+if (module.hot) {
+  module.hot.accept('./client/Root', () => { render(Root) })
+}
+
+
 registerServiceWorker();
